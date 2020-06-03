430c76bbc28a553cc7930f1da69224d3c1038912
Amy Fabella <amyfabella@hotmail.com>
2020-06-02 19:23:32 -0700

    started script

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,32 +3,32 @@ const db = require("../models");
 // Defining methods for the Controller
 module.exports = {
   findAll: function(req, res) {
-    db.Model
+    db.Plant
       .find(req.query)
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.Book
+    db.Plant
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    db.Book
+    db.Plant
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.Book
+    db.Plant
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.Book
+    db.Plant
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
