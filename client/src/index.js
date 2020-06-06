e589afbe391f745d85c0bef47ef45473ea322788
Amy Fabella <amyfabella@hotmail.com>
2020-06-05 21:07:45 -0700

    changed cultivate to client

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+// import { Button, DatePicker, version } from "antd";
+import "antd/dist/antd.css";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
